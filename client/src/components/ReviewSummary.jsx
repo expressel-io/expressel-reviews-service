965f8a6ff886982a5d9bb6e1643128d7303d4f82
Courtney Chao <couchao@ucdavis.edu>
2018-07-19 15:32:53 -0700

    Add html skeleton for ReviewSummary component

--- client/src/components/ReviewSummary.jsx
+++ client/src/components/ReviewSummary.jsx
@@ -1,1 +1,29 @@
 const React = require('react');
+const ReactDOM = require('react-dom');
+
+const ReviewSummary = (props) => {
+  return { 
+  	<div class='ReviewSummary'>
+  	<h2>Reviews Summary</h2>
+      <div id="Bars">
+        <div id="5Stars"> 5 STAR BAR HERE </div>
+        <div id="4Stars"> 4 STAR BAR HERE </div>
+        <div id="3Stars"> 3 STAR BAR HERE </div>
+        <div id="2Stars"> 2 STAR BAR HERE </div>
+        <div id="1Star"> 1 STAR BAR HERE </div>
+      </div>
+      <div id="AggregatedReviews">
+        <div id="AvgRating"> NUMBER HERE </div>
+        <div id="AvgStars"> STARS HERE </div>
+        <div id="TotalReviews">2,129 reviews</div>
+      </div>
+      <div>
+        <div id="Ease"> <span class="title"> Ease of Use </span> text from a review here</div>
+        <div id="Value"> <span class="title"> Value </span> text from a review here</div>
+        <div id="Design/Style"> <span class="title"> Design/Style </span> text from a review here</div>
+      </div>
+  	</div>
+  }
+};
+
+export default ReviewSummary;
