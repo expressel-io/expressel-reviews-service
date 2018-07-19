b72ad5c36dd00476edb32e7c174dea6cf579850d
Courtney Chao <couchao@ucdavis.edu>
2018-07-19 15:10:30 -0700

    Connect database

--- db/database.js
+++ db/database.js
@@ -1,13 +1,37 @@
 const faker = require('faker');
+const mysql = require('mysql');
 
-const sources = ['Newegg.com', 'Costco.com', 'Target.com', 'Ebay.com', 'Walmart.com', ''];
+// Create and connect database
 
-const dataGenerator = () => {
-  //Title
-  //Date
-  //Star Rating
-  //Review Text 
-  //Source
+dbConnection = mysql.createConnection({
+  user: 'root',
+  password: null,
+  database: 'reviews'
+});
+
+dbConnection.connect();
+
+// Generate fake reviews
+
+const sources = ['Newegg.com', 'Costco.com', 'Target.com', 'Ebay.com', 'Walmart.com'];
+
+const generateReviews = () => { 
+  {
+    rating: Math.floor( Math.Random() * 5 ),
+    title: faker.lorem.sentence(),
+    date: faker.date.past(),
+    text: faker.lorem.paragraphs(),
+    source: sources[Math.floor(Math.Random() * 5)]    
+  }
+} 
+
+const addReviews = () => {
+  for (let i = 0; i < 100; i += 1) {
+    let query = 'INTERT INTO itemReviews (rating, title, date, text, source) VALUES (? ? ? ? ?)';
+    
+    generateReviews()
+  }
+  
 }
 
-export
+//Add generated data to the database\\
