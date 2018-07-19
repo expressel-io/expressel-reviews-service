464ce9edc81e164be172bc37fef265e17c8f5b57
Courtney Chao <couchao@ucdavis.edu>
2018-07-18 19:42:55 -0700

    webpack and other config setup

---
+++ db/database.js
@@ -0,0 +1,13 @@
+const faker = require('faker');
+
+const sources = ['Newegg.com', 'Costco.com', 'Target.com', 'Ebay.com', 'Walmart.com', ''];
+
+const dataGenerator = () => {
+  //Title
+  //Date
+  //Star Rating
+  //Review Text 
+  //Source
+}
+
+export
