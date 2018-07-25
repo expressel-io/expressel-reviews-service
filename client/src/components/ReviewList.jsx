9b19381826285abf268b48fa91c9f083462e0d3d
Courtney Chao <couchao@ucdavis.edu>
2018-07-25 14:54:34 -0700

    Add clickHandler to ReviewList

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -1,29 +1,47 @@
 import React from 'react';
+import axios from 'axios';
 import IndividualReviews from './IndividualReviews.jsx';
 
 class ReviewList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reviews: []
+      reviews: [],
+      itemId: 1,
     };
     this.getAllReviews = this.getAllReviews.bind(this);
     this.handleGetAllReviewsClick = this.handleGetAllReviewsClick.bind(this);
+    this.getFirstReviews = this.getFirstReviews.bind(this);
+  }
+
+  getFirstReviews() {
+    axios.get(`/api/reviews/first/${this.state.itemId}`)
+      .then((response) => {
+        this.setState({
+          reviews: response.data.map(reviews => reviews)
+        });
+      });
   }
 
   getAllReviews() {
-    console.log('In get all reviews!')
+    axios.get(`/api/reviews/${this.state.itemId}`)
+      .then((response) => {
+        this.setState({
+          reviews: response.data.map(reviews => reviews)
+        });
+      });
+    console.log('In get all reviews!');
   }
 
   handleGetAllReviewsClick(e) {
     console.log('clicked!', e);
-    getAllReviews();
+    this.getAllReviews();
   }
 
   render() {
     return (
       <div>
-        {this.state.reviews.map((review) => { return <IndividualReviews review={review} />; })}
+        {this.state.reviews.map((review) => { return <IndividualReviews review={review} onClick={this.handleGetAllReviewsClick}/>; })}
       </div>
     );
   }
