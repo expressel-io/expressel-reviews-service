1ef878821a7c4e2a19159b203fb2618341e08c2d
Courtney Chao <couchao@ucdavis.edu>
2018-07-25 11:11:59 -0700

    Add functions for API calls

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+
+const bodyParser = require('body-parser');
+const db = require('../db/database.js');
+
 const app = express();
-const path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
 // set up environmental variable
 const PORT = process.env.PORT || 3002;
@@ -13,6 +22,30 @@ app.get(() => {
   console.log('Get is working!');
 });
 
-// start server
+// API calls
 
+app.get('/api/reviews', (req, res) => {
+  db.getAllReviews((error, results) => {
+    if (error) {
+      console.log('Error getting the reviews: ', error);
+    } else {
+      console.log('getting reviews');
+      res.send(results);
+    }
+  });
+});
+
+app.get('/api/reviews/:itemId', (req, res) => {
+  console.log(req.params.itemId, 'itemId');
+  db.getFirstReviews([req.params.itemId], (error, results) => {
+    if (error) {
+      console.log('Error getting the first reviews: ', error);
+    } else {
+      console.log('getting first reviews');
+      res.send(results);
+    }
+  });
+});
+
+// start server
 app.listen(PORT, () => { console.log(`Listening at ${PORT}!`); });
