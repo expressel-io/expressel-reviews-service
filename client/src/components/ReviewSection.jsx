129847a54b17e3737701a4a5a596cf382b74e314
Courtney Chao <couchao@ucdavis.edu>
2018-07-27 08:32:37 -0700

    Refactor api endpoints

---
+++ client/src/components/ReviewSection.jsx
@@ -0,0 +1,76 @@
+const React = require('react');
+const axios = require('axios');
+const ReviewSummary = require('./ReviewSummary.jsx');
+const ReviewList = require('./ReviewList.jsx');
+
+class ReviewSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: [],
+      itemId: 1,
+
+    };
+    this.getAllReviews = this.getAllReviews.bind(this);
+    this.handleGetAllReviewsClick = this.handleGetAllReviewsClick.bind(this);
+    this.getFirstReviews = this.getFirstReviews.bind(this);
+    this.getAvgRating = this.getAvgRating.bind(this);
+  }
+
+  componentDidMount() {
+    this.getFirstReviews();
+  }
+
+  getFirstReviews() {
+    axios.get(`/api/${this.state.itemId}/reviews/first`)
+      .then((response) => {
+        this.setState({
+          reviews: response.data.map(reviews => reviews),
+        });
+      })
+      .catch((error) => {
+        console.log('There was an error getting the initial reviews: ', error);
+      });
+  }
+
+  getAllReviews() {
+    axios.get(`/api/${this.state.itemId}/reviews`)
+      .then((response) => {
+        this.setState({
+          reviews: response.data.map(reviews => reviews),
+        });
+      })
+      .catch((error) => {
+        console.log('There was an error getting all reviews: ', error);
+      });
+    console.log('In get all reviews!');
+  }
+
+  getAvgRating() {
+    axios.get(`/api/${this.state.itemId}/reviews/avg`)
+      .then((response) => {
+        this.setState({
+          average: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log('There was an error getting the average rating: ', error);
+      });
+  }
+
+  handleGetAllReviewsClick(e) {
+    console.log('clicked!', e);
+    this.getAllReviews();
+  }
+
+  render() {
+    return (
+      <div>
+        <ReviewSummary reviews={ this.state.reviews } average={ this.state.average } />
+        <ReviewList reviews={ this.state.reviews } onClick={ this.handleGetAllReviewsClick } />
+      </div>
+    );
+  }
+}
+
+export default ReviewSection;
