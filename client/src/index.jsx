f02efc1bc04a1b2fc224d3aef16496802a7e4b34
Courtney Chao <couchao@ucdavis.edu>
2018-07-26 22:59:22 -0700

    Clean index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
+import React from 'react';
 import ReactDOM from 'react-dom';
 import ReviewSection from './components/ReviewSection.jsx';
 
 ReactDOM.render(<ReviewSection />, document.getElementById('Reviews'));
-=======
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Reviews from './components/Reviews';
-
-ReactDOM.render(<Reviews />, document.getElementById('Reviews'));
->>>>>>> master
