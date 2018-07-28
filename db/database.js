92943b0c1015a5aebb72ffb6de4e88d454403a77
Courtney Chao <couchao@ucdavis.edu>
2018-07-27 23:13:58 -0700

    Fix database bug

--- db/database.js
+++ db/database.js
@@ -20,7 +20,7 @@ connection.connect();
 const sources = ['Newegg.com', 'Costco.com', 'Target.com', 'Ebay.com', 'Walmart.com'];
 
 const generateReviews = (itemId) => {
-  const randomNumber = Math.floor(Math.random() * Math.floor(100));
+  const randomNumber = Math.floor(Math.random() * Math.floor(50));
   for (let i = 0; i < randomNumber; i += 1) {
     const rating = Math.floor(Math.random() * 5);
     const title = faker.lorem.sentence();
@@ -28,7 +28,7 @@ const generateReviews = (itemId) => {
     const text = faker.lorem.paragraphs();
     const source = sources[Math.floor(Math.random() * 5)];
     const item = itemId;
-    const query = 'INSERT INTO itemReviews (id, rating, title, date, text, source, item_id) VALUES (null, ?, ?, ?, ?, ?, ?)';
+    const query = 'INSERT INTO itemReviews (rating, title, date, text, source, item_id) VALUES (?, ?, ?, ?, ?, ?);';
     connection.query(query, [rating, title, date, text, source, item], (err, results) => {
       if (err) {
         console.log('error adding values in table: ', err);
@@ -43,7 +43,7 @@ const generateReviews = (itemId) => {
 // Add generated data to the database
 const generateItems = (num) => {
   for (let item = 2; item < num; item += 1) {
-    connection.query('INSERT INTO items (id, name) VALUES (item, ?)', [faker.lorem.words()]);
+    connection.query(`INSERT INTO items (id, name) VALUES (${item}, ?)`, [faker.lorem.words()]);
     generateReviews(num);
   }
 };
--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,11 @@
-CREATE DATABASE IF NOT EXISTS reviews;
+DROP DATABASE IF EXISTS reviews;
+
+CREATE DATABASE reviews;
 
 USE reviews;
 
-CREATE TABLE items (
-  id INT NOT NULL,
-  name VARCHAR(50),
-  PRIMARY KEY (id)
-);
+CREATE TABLE items ( id INT NOT NULL, name VARCHAR(100), PRIMARY KEY (id));
 
-CREATE TABLE itemReviews (
-  id INT NOT NULL AUTO_INCREMENT,
-  rating INT NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  date DATE NOT NULL,
-  text VARCHAR(2000),
-  source VARCHAR(40) NOT NULL,
-  item_id INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (item_id) REFERENCES items(id)
-);
+CREATE TABLE itemReviews ( id INT NOT NULL AUTO_INCREMENT, rating INT NOT NULL, title VARCHAR(100) NOT NULL, date DATE NOT NULL, text VARCHAR(2000), source VARCHAR(40) NOT NULL, item_id INT NOT NULL, PRIMARY KEY (id));
 
 
