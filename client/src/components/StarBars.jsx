6c3756360b08be35ed8b6b40dd5aec270f854303
courtchao <couchao@ucdavis.edu>
2018-08-04 11:37:32 -0700

    Clean comments

--- client/src/components/StarBars.jsx
+++ client/src/components/StarBars.jsx
@@ -2,17 +2,6 @@ const React = require('react');
 
 import StarBarFiller from './StarBarFiller';
 
-// map through the number of expected bars (5)
-// for each one place a grey bar same width
-// place a gold bar relative to the amount of ratings re: total ratings on top of the grey bar
-// place the star value left of the grey bar
-// place percentage right side of the grey bar
-
-// find total of n star reviews
-// have review bar full length (40%)
-
-// {props.STARSUM.map((bar) => { return (<div class="bar"><div>}))}
-
 const StarBars = (props) => {
   const {
     ratingsCounts
